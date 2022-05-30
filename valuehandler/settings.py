56c5bb5deec17739099b74d5f82848052492f192
MubarakOke <okeolamubarak18@gmail.com>
2022-05-30 12:38:06 +0100

    initial commit

--- valuehandler/settings.py
+++ valuehandler/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['127.0.0.1', 'valuhandler.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'valuehandler.herokuapp.com']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
