66f5de5caaf375b9160f79f45aaa372456b3c5f1
joyinola <damilolaadetoro1@gmail.com>
2022-06-06 18:13:37 +0100

    'userlist'

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import RegistrationView, AuthenticateAdminView, AuthenticateSuperAdminView
+from accounts.views import RegistrationView,UserListView,EditUserRoleView, AuthenticateAdminView, AuthenticateSuperAdminView
 
 app_name= 'accounts'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('auth/superadmin/', AuthenticateSuperAdminView.as_view(), name='auth_superadmin'),
     path('auth/admin/', AuthenticateAdminView.as_view(), name='auth_admin'),
+    path('users/',UserListView.as_view(),name='list_users'),
+    path('edit/',EditUserRoleView.as_view(),name='edit_role'),
 ]
